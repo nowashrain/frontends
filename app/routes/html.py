af1c055cec1cfaf7bb20db022e135342aba5169e
[nowashrain] <[rltkanth@gmail.com]>
2024-07-25 18:18:59 +0900

    html5 - 태그, 요소, 속성, 문서구조

--- app/routes/html.py
+++ app/routes/html.py
@@ -1,12 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from starlette.templating import Jinja2Templates
 
-
-# 라우터 생성
+# HTML 관련 라우트를 위한 APIRouter 인스턴스 생성
 html_router = APIRouter()
-# 템플릿 지정
+
+# 템플릿 설정
 templates = Jinja2Templates(directory='views/templates')
 
-# 라우트 설정
+# HTML 관련 뷰의 홈 라우트
+@html_router.get('/')
+async def home(req: Request):
+    # HTML 인덱스 페이지 렌더링
+    return templates.TemplateResponse('html/index.html', {'request': req})
 
-
+# 레이아웃 예제 라우트
+@html_router.get('/layout')
+async def show_layout(req: Request):
+    # 레이아웃 예제 페이지 렌더링
+    return templates.TemplateResponse('html/01layout.html', {'request': req})
