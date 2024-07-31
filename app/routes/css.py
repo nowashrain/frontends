a52c9b0a70f1d1603f3dba8bba9e25e6a4c0bbb3
[nowashrain] <[rltkanth@gmail.com]>
2024-07-31 12:19:40 +0900

    css3 - 선택자, CSS리셋

--- app/routes/css.py
+++ app/routes/css.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from starlette.templating import Jinja2Templates
 
 
@@ -7,5 +7,20 @@ css_router = APIRouter()
 # 템플릿 지정
 templates = Jinja2Templates(directory='views/templates')
 
-# 라우트 설정
 
+@css_router.get('/')
+async def semantic(req: Request):
+    return templates.TemplateResponse('css/index.html', {'request': req})
+
+
+@css_router.get('/selector')
+async def selector(req: Request):
+    return templates.TemplateResponse('css/01selector.html', {'request': req})
+
+@css_router.get('/reset')
+async def reset(req: Request):
+    return templates.TemplateResponse('css/02reset.html', {'request': req})
+
+@css_router.get('/text')
+async def text(req: Request):
+    return templates.TemplateResponse('css/03text.html', {'request': req})
