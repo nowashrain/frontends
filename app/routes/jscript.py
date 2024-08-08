a45a4a35914679babcc57d68d9de275d347a01be
[nowashrain] <[rltkanth@gmail.com]>
2024-08-08 11:56:49 +0900

    콜백함수, 내장함수

--- app/routes/jscript.py
+++ app/routes/jscript.py
@@ -45,3 +45,7 @@ async def array(req: Request):
 @jscript_router.get('/function')
 async def fucntion(req: Request):
     return templates.TemplateResponse('js/08function.html', {'request': req})
+
+@jscript_router.get('/callback')
+async def callback(req: Request):
+    return templates.TemplateResponse('js/09callback.html', {'request': req})
